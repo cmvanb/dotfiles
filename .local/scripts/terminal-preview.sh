77989eeb02d4be5d2ef5cf75716853e53fc92538
Casper <draakie@protonmail.com>
2023-08-16 15:54:16 +0200

    scripts: Update terminal previewer bat style.

--- .local/scripts/terminal-preview.sh
+++ .local/scripts/terminal-preview.sh
@@ -8,6 +8,6 @@ set -o pipefail
 
 case "$1" in
     *.jpg|*.jpeg|*.png) chafa "$1" -f sixel -s "$(($2-2))x$3" | sed 's/#/\n#/g' ;;
-    *) bat --force-colorization --paging=never --style=changes,numbers \
+    *) bat --force-colorization --paging=never --style=numbers \
             --wrap never -f "$1" && false ;;
 esac
