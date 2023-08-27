4d9ce230ad38fcf3e816bc8f50b5011c88735e46
Casper <draakie@protonmail.com>
2023-08-27 20:19:26 +0200

    river: Lookup binary instead of hardcoding it.

--- .config/river/river-run.sh
+++ .config/river/river-run.sh
@@ -9,10 +9,10 @@ set -o pipefail
 source $XDG_CONFIG_HOME/river/environment.sh
 
 # Ensure data directory exists.
-if [[ ! -d $XDG_DATA_HOME/river ]] ; then
+if [[ ! -d $XDG_DATA_HOME/river ]]; then
     mkdir -p $XDG_DATA_HOME/river
 fi
 
 # Run river and save session log file.
-/run/current-system/sw/bin/river 2> $XDG_DATA_HOME/river/session_log
+river 2> $XDG_DATA_HOME/river/session_log
 
