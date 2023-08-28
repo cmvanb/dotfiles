240cb003e55a479b45eb92897ea14394c12c2043
Casper <draakie@protonmail.com>
2023-08-28 23:46:52 +0200

    scripts: Add select bookmark script.

---
+++ .local/scripts/select-bookmark.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------------------------------
+# Select an existing bookmark
+#-------------------------------------------------------------------------------
+
+set -euo pipefail
+
+# Validation
+#-------------------------------------------------------------------------------
+
+if ! command -v wofi &> /dev/null; then
+    echo "ERROR: "$(basename "$0")" missing dependency: wofi"
+    exit
+fi
+
+# Select a bookmark
+#-------------------------------------------------------------------------------
+
+bookmarks_dir=$HOME/Wiki/bookmarks
+
+bookmarks=$(fd . "$bookmarks_dir" --max-depth 1 --exec basename)
+
+# TODO: Use bookmark title from YAML frontmatter instead of file name.
+# NOTE: Performance of `yq` in this loop is seriously degraded compared to
+# being run in bare shell. Needs investigation. `awk` performance is no better.
+# i=0
+# for b in $bookmarks; do
+#     (( i=i+1 ))
+#     # see: https://github.com/kislyuk/yq/issues/151
+#     # bookmark_title=$(awk '{ if ($1 == "---" || $1 == "...") n+=1; if(n>1) exit 0; print; }' $b | yq -Mr -- ".aliases[]")
+#     bookmark_title=$(yq -Mr -- ".aliases[]" $b 2> /dev/null)
+#     echo "$i: $bookmark_title"
+# done
+
+bookmark=$(echo "$bookmarks" | wofi --prompt "Open bookmark" --show dmenu 2> /dev/null)
+
+bookmark_file="$bookmarks_dir/$bookmark"
+
+url=$(rg --only-matching --no-line-number "\(([^}]*)\)" $bookmark_file)
+url=${url:1:-1}
+
+qutebrowser --target window $url 2> /dev/null
