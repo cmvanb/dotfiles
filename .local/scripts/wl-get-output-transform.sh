37200d172e965742350adac68cd05b5dd489ba12
Casper <draakie@protonmail.com>
2023-08-29 13:37:02 +0200

    scripts: Improve validation for scripts interacting with wayland outputs.

--- .local/scripts/wl-get-output-transform.sh
+++ .local/scripts/wl-get-output-transform.sh
@@ -3,11 +3,17 @@
 # Get the transform of a wayland output.
 #-------------------------------------------------------------------------------
 
-set -o nounset
-set -o pipefail
+set -uo pipefail
+
+output=$(wlr-randr | grep -m1 -n -w $1)
+
+if [[ -z $output ]]; then
+    echo "ERROR: output named \`$1\` not found"
+    exit 1
+fi
 
 # Find the line number on which the display info begins.
-line_number=$(wlr-randr | grep -m1 -n $1 | cut -d: -f1)
+line_number=$(echo $output | cut -d: -f1)
 
 # Tail the output from that line and match the first `Transform` line, then slice that match on spaces.
 transform=($(wlr-randr | tail -n +"$line_number" | awk '/Transform/{print $line; exit}'))
