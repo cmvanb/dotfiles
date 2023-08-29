e335150ac5d469fc890d4cf31a14387022edb302
Casper <draakie@protonmail.com>
2023-08-29 23:02:56 +0200

    river: XDG desktop portal integration.

--- .config/river/environment.sh
+++ .config/river/environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-set -o nounset
-set -o pipefail
-
 # TODO: This should be part of the river/systemd integration.
-
 # Tells libseat what login daemon we're using so it doesn't have to cry about it.
 export LIBSEAT_BACKEND=logind
 
+# TODO: This should be part of the wayland/QT integration.
 # QT performance flag.
 export QT_QPA_PLATFORM=wayland
+
+# XDG desktop portal integration.
+export XDG_CURRENT_DESKTOP=river
