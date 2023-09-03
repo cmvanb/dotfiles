606e145c0e3a895b3a7226d7d6edfba53cc918f9
Casper <draakie@protonmail.com>
2023-09-03 21:44:23 +0200

    scripts: Improve force_link error handling.

--- .local/scripts/fs-utils.sh
+++ .local/scripts/fs-utils.sh
@@ -5,15 +5,21 @@
 force_link () {
     if [[ -z "$1" ]]; then
         echo "[$(basename "$0")] ERROR: Missing argument: TARGET"
+        return 1
     fi
     if [[ -z "$2" ]]; then
         echo "$(basename "$0") ERROR: Missing argument: LINK_NAME"
+        return 1
+    fi
+    if [[ ! -f "$1" && ! -d "$1" ]]; then
+        echo "[$(basename "$0")] ERROR: Tried to link non-existent \`$1\`"
+        return 1
     fi
 
+    # Delete pre-existing directory if we are trying to link it.
     if [[ -d "$2" ]]; then
         rm -rf "$2"
-    elif [[ ! -f "$1" ]]; then
-        echo "[$(basename "$0")] ERROR: Tried to link non-existent \`$1\`"
     fi
+
     ln -sfT "$1" "$2"
 }
