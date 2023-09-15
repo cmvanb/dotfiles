a63ab56ba568f55a6c090ece90b13cd57a6e7264
Casper <draakie@protonmail.com>
2023-09-15 11:27:09 +0200

    lf: Exit preview with `h`, use preview for text files with `l`.

---
+++ .config/lf/lf-open.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------------------------------
+# Open a file from LF
+#-------------------------------------------------------------------------------
+
+if ! command -v file &> /dev/null; then
+    echo "["$(basename "$0")"] ERROR: Missing dependency: file"
+    exit 1
+fi
+
+if [[ -z "$1" ]]; then
+    echo "[$(basename "$0")] ERROR: Missing argument: file name"
+    return 1
+fi
+
+mime="$(file -b --mime-type "$(realpath "$1")")"
+
+if [[ "$mime" =~ "text" ]]; then
+    $XDG_SCRIPTS_HOME/terminal-preview.sh $1 | less -R --lesskey-file=$XDG_CONFIG_HOME/lf/lf-less.lesskey
+else
+    xdg-open $1 &
+fi
