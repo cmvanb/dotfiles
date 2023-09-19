448d00a2f7b98478cfb17ddc3f4a2e450700c75b
Casper <draakie@protonmail.com>
2023-09-19 11:19:33 +0200

    scripts: Add terminal pdf preview.

--- .local/scripts/terminal-preview.sh
+++ .local/scripts/terminal-preview.sh
@@ -5,13 +5,36 @@
 
 set -euo pipefail
 
+# Validation
+#-------------------------------------------------------------------------------
+
+if ! command -v chafa &> /dev/null; then
+    echo "["$(basename "$0")"] ERROR: Missing dependency: chafa"
+    exit 1
+fi
+
+if ! command -v gs &> /dev/null; then
+    echo "["$(basename "$0")"] ERROR: Missing dependency: gs"
+    exit 1
+fi
+
+if ! command -v bat &> /dev/null; then
+    echo "["$(basename "$0")"] ERROR: Missing dependency: bat"
+    exit 1
+fi
+
 if [[ -z "$1" ]]; then
     echo "[$(basename "$0")] ERROR: Missing argument: file name"
     exit 1
 fi
 
+# Choose a file preview method based on file name extension
+#-------------------------------------------------------------------------------
+
 case "$1" in
     *.jpg|*.jpeg|*.png) chafa "$1" -f sixel -s "$(($2-2))x$3" | sed 's/#/\n#/g' ;;
+    *.pdf) gs -q -dNOPAUSE -dBATCH -sDEVICE=jpeg -r240 -sOutputFile=- \
+        -dLastPage=1 "$1" | chafa -f sixel -s "$(($2-2))x$3" | sed 's/#/\n#/g' ;;
     *) bat --force-colorization --paging=never --style=numbers --wrap never \
         -f "$1" ;;
 esac
