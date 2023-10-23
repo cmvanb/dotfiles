c20ff5ce1ff6a065d5116fa83fc1e83307b15b77
Casper <draakie@protonmail.com>
2023-10-23 08:46:26 +0200

    deploy: Enable pipewire and syncthing services.

--- deploy/enable-systemd-services.sh
+++ deploy/enable-systemd-services.sh
@@ -13,7 +13,5 @@ echo "Enabling systemd user services."
 systemctl --user enable mako
 systemctl --user enable ssh-agent
 systemctl --user enable udiskie
-
-# TODO: Figure out cross platform deployment for these.
-# systemctl --user enable pipewire
-# systemctl --user enable syncthing
+systemctl --user enable pipewire
+systemctl --user enable syncthing
