04254a83554d143f1c75c0935ec4979f70a8896f
Casper <draakie@protonmail.com>
2023-10-30 15:31:49 +0100

    scripts: Moved get-distro-id to a utility function.

---
+++ .local/scripts/linux-utils.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------------------------------
+# Linux specific utilities
+#-------------------------------------------------------------------------------
+
+set -euo pipefail
+
+bash_dir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+
+# Get the currently running linux distribution ID.
+#-------------------------------------------------------------------------------
+
+get_distro_id () {
+    os_release_path=/etc/os-release
+
+    if [[ ! -f "$os_release_path" ]]; then
+        echo "[$(basename "$0")] ERROR: \`$os_release_path\` does not exist. Unable to get distribution ID."
+        exit 1
+    fi
+    source $bash_dir/parse-conf.sh
+
+    parse_conf "$os_release_path"
+
+    declare -n result=conf_vars
+
+    echo "${result[ID]}"
+}
