d51ad0c00f8aa1fdbea936523b11296604d892e9
Casper <draakie@protonmail.com>
2024-01-19 12:41:10 +0100

    fuzzel: Complete switch from wofi to fuzzel.

--- .local/scripts/select-bookmark.sh
+++ .local/scripts/select-bookmark.sh
@@ -14,7 +14,7 @@ source "$XDG_OPT_HOME/shell-utils/debug.sh"
 # Validation
 #-------------------------------------------------------------------------------
 
-assert_dependency wofi
+assert_dependency fuzzel
 
 # Select a bookmark
 #-------------------------------------------------------------------------------
@@ -37,7 +37,7 @@ bookmarks=$(fd . "$bookmarks_dir" --max-depth 1 --exec basename)
 # done
 
 declare bookmark
-bookmark=$(echo "$bookmarks" | wofi --prompt "Open bookmark" --show dmenu 2> /dev/null)
+bookmark=$(echo "$bookmarks" | fuzzel --dmenu 2> /dev/null)
 
 declare bookmark_file
 bookmark_file="$bookmarks_dir/$bookmark"
