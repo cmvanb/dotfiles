165f7aa87d07567cb78c5d3546c6db27a9beffed
Casper <draakie@protonmail.com>
2024-02-22 23:23:01 +0100

    lf: Open text files with bat directly instead of terminal-preview script.

--- .config/lf/lf-open.sh
+++ .config/lf/lf-open.sh
@@ -27,7 +27,8 @@ encoding=$(file_encoding "$1")
 
 if [[ $mimetype == "text"* || $mimetype == "application/javascript" || $encoding == *"ascii" || $encoding == "utf-8" ]]; then
     "$XDG_SCRIPTS_HOME/set-terminal-title.sh" "${1/$HOME/\~}"
-    "$XDG_SCRIPTS_HOME/terminal-preview.sh" "$1" | less -R --chop-long-lines --lesskey-file="$XDG_CONFIG_HOME/lf/lf-less.lesskey"
+    bat --style=numbers --force-colorization --paging=never --wrap=never "$1" \
+        | less -R --chop-long-lines --lesskey-file="$XDG_CONFIG_HOME/lf/lf-less.lesskey"
     "$XDG_SCRIPTS_HOME/set-terminal-title.sh" "${PWD/$HOME/\~}"
 else
     nohup xdg-open "$1" > /dev/null 2>&1 &
