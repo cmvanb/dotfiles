557d5534860c3d88fd34ddc614e3b4afafaf85ad
Casper <draakie@protonmail.com>
2024-04-10 01:58:57 +0200

    [WIP]

--- .local/scripts/select-bookmark.sh
+++ .local/scripts/select-bookmark.sh
@@ -22,17 +22,17 @@ assert_dependency wofi
 declare bookmarks_dir=$HOME/Wiki/bookmarks
 
 declare bookmarks
-bookmarks=$(fd . "$bookmarks_dir" --max-depth 1 --exec basename)
+bookmarks=$(fd . "$bookmarks_dir" --max-depth 1 --exec head -4 "{}" | yq -r '.aliases[]')
 
-# TODO: Use bookmark title from YAML frontmatter instead of file name.
-# NOTE: Performance of `yq` in this loop is seriously degraded compared to
-# being run in bare shell. Needs investigation. `awk` performance is no better.
+# # TODO: Use bookmark title from YAML frontmatter instead of file name.
+# # NOTE: Performance of `yq` in this loop is seriously degraded compared to
+# # being run in bare shell. Needs investigation. `awk` performance is no better.
 # i=0
 # for b in $bookmarks; do
 #     (( i=i+1 ))
 #     # see: https://github.com/kislyuk/yq/issues/151
 #     # bookmark_title=$(awk '{ if ($1 == "---" || $1 == "...") n+=1; if(n>1) exit 0; print; }' $b | yq -Mr -- ".aliases[]")
-#     bookmark_title=$(yq -Mr -- ".aliases[]" $b 2> /dev/null)
+#     bookmark_title=$(head -4 "$b" | yq -r '.aliases[]')
 #     echo "$i: $bookmark_title"
 # done
 
