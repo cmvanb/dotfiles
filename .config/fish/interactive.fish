410eca524f8d8e65092597f78bf79a7c44b57d91
Casper <draakie@protonmail.com>
2024-04-10 16:51:04 +0200

    shell: Add git shortcut for `git switch`; minor cleanup.

--- .config/fish/interactive.fish
+++ .config/fish/interactive.fish
@@ -24,11 +24,6 @@ set -x PYTHONSTARTUP $XDG_CONFIG_HOME/python/interactive.py
 alias eza "eza -l --color=always --group-directories-first --time-style=long-iso"
 alias bat "bat --force-colorization --no-paging --style=grid,numbers"
 
-abbr -a gs git status
-abbr -a gsu git status -u
-abbr -a gl git log --pretty=history
-abbr -a gd git diff
-abbr -a gds git diff --staged
 abbr -a ga git add
 abbr -a gaa git add -A
 abbr -a gai git add -i
@@ -36,11 +31,18 @@ abbr -a gb git branch -v
 abbr -a gc git commit -m
 abbr -a gca git commit --amend
 abbr -a gco git checkout
+abbr -a gd git diff
+abbr -a gds git diff --staged
+abbr -a gl git log --pretty=history
 abbr -a gp git push
 abbr -a gpf git push --force
+abbr -a gpu git pull --rebase
+abbr -a gr git remote -v
 abbr -a grb git rebase -i
 abbr -a grc git rm --cached
-abbr -a grv git remote -v
+abbr -a gs git status
+abbr -a gsu git status -u
+abbr -a gsw git switch
 abbr -a e edit
 abbr -a ed edit
 abbr -a edi edit
