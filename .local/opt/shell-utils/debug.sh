a64396c2b0c72ff1a05a1c16f9da48129fdc211d
Casper <draakie@proton.me>
2024-04-16 13:04:14 +0200

    debug-utils: Echo assert_dependency() error to STDERR.

--- .local/opt/shell-utils/debug.sh
+++ .local/opt/shell-utils/debug.sh
@@ -4,7 +4,8 @@
 
 assert_dependency () {
     if ! command -v "$1" &> /dev/null; then
-        echo "[$(basename "$0")] ERROR: Missing dependency: $1"
-        exit 1
+        >&2 echo "[$(basename "$0")] ERROR: Missing dependency: $1"
+        return 1
     fi
+    return 0
 }
