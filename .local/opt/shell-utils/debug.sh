105e4b317821cd13cc4fe09d6b8935758ad76032
Casper <draakie@proton.me>
2024-04-16 11:55:31 +0200

    scripts: Shellcheck fix for assert_dependency().

--- .local/opt/shell-utils/debug.sh
+++ .local/opt/shell-utils/debug.sh
@@ -4,7 +4,7 @@
 
 assert_dependency () {
     if ! command -v "$1" &> /dev/null; then
-        echo "["$(basename "$0")"] ERROR: Missing dependency: $1"
+        echo "[$(basename "$0")] ERROR: Missing dependency: $1"
         exit 1
     fi
 }
