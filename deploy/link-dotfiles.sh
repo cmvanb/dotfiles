4a84d823d255ae1337aa67ea63ce46caee8ddf5d
Casper <draakie@proton.me>
2024-04-18 19:06:43 +0200

    deploy: Minor clean up.

--- deploy/link-dotfiles.sh
+++ deploy/link-dotfiles.sh
@@ -69,9 +69,9 @@ force_link "$base_dir/.local/scripts/screenshot-rectangle.sh" "$scripts_dir/scre
 force_link "$base_dir/.local/scripts/select-bookmark.sh" "$scripts_dir/select-bookmark.sh"
 force_link "$base_dir/.local/scripts/set-terminal-title.sh" "$scripts_dir/set-terminal-title.sh"
 force_link "$base_dir/.local/scripts/show-path.sh" "$scripts_dir/show-path.sh"
+force_link "$base_dir/.local/scripts/show-webcam.sh" "$scripts_dir/show-webcam.sh"
 force_link "$base_dir/.local/scripts/terminal-preview.sh" "$scripts_dir/terminal-preview.sh"
 force_link "$base_dir/.local/scripts/view.sh" "$scripts_dir/view.sh"
-force_link "$base_dir/.local/scripts/show-webcam.sh" "$scripts_dir/show-webcam.sh"
 
 # Shell libraries
 force_link "$base_dir/.local/opt/shell-utils" "$opt_dir/shell-utils"
