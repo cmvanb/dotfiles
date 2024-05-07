af5aaaa62d927a225b35ac32225fe1bdb0873f50
Casper <draakie@proton.me>
2024-05-08 00:17:41 +0200

    river: Add new terminal in directory shortcut; minor clean up.

--- .config/river/keymaps.sh
+++ .config/river/keymaps.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 #-------------------------------------------------------------------------------
 # River key mappings
+#
+# See all valid mappings here: `/usr/include/xkbcommon/xkbcommon-keysyms.h`
 #-------------------------------------------------------------------------------
 
 declare config_dir=${XDG_CONFIG_HOME:-$HOME/.config}
@@ -19,20 +21,30 @@ source "$opt_dir/shell-utils/debug.sh"
 assert_dependency alacritty
 assert_dependency makoctl
 assert_dependency qutebrowser
+assert_dependency riverctl
+assert_dependency river-shifttags
 assert_dependency wofi
 
-# Key mappings
+# System mappings
 #-------------------------------------------------------------------------------
-# See all valid mappings here: `/usr/include/xkbcommon/xkbcommon-keysyms.h`
+
+# Lock river
+riverctl map normal Alt+Shift Z spawn "$scripts_dir/lock-screen.sh"
+
+# Exit river
+riverctl map normal Alt+Shift Q exit
+
+# Shortcut mappings
+#-------------------------------------------------------------------------------
+
+# Run menu
+riverctl map normal Alt O spawn "wofi --show drun"
 
 # New terminal
 riverctl map normal Alt T spawn "alacritty"
 
-# Process manager
-riverctl map normal Alt M spawn "alacritty --command htop"
-
-# Command menu
-riverctl map normal Alt O spawn "wofi --show drun"
+# New terminal in a directory
+riverctl map normal Alt+Shift T spawn "$scripts_dir/open-terminal-cwd.sh"
 
 # Browser
 riverctl map normal Alt B spawn "qutebrowser"
@@ -40,17 +52,23 @@ riverctl map normal Alt B spawn "qutebrowser"
 # Browser session
 riverctl map normal Alt+Shift B spawn "$scripts_dir/open-qutebrowser-session.sh"
 
-# Dismiss notifications
-riverctl map normal Alt D spawn "makoctl dismiss -a"
+# Process manager
+riverctl map normal Alt M spawn "alacritty --command htop"
+
+# View mappings
+#-------------------------------------------------------------------------------
+
+# Toggle view float
+riverctl map normal Alt Space toggle-float
+
+# Toggle view fullscreen
+riverctl map normal Alt F toggle-fullscreen
 
 # Close view
 riverctl map normal Alt Q close
 
-# Exit river
-riverctl map normal Alt+Shift Q exit
-
-# Lock system
-riverctl map normal Alt+Shift Z spawn "$scripts_dir/lock-screen.sh"
+# Bump the focused view to the top of the layout stack
+riverctl map normal Alt Z zoom
 
 # Focus the next/previous view in the layout stack
 riverctl map normal Alt J focus-view next
@@ -71,12 +89,20 @@ riverctl map normal Alt+Shift L spawn "$config_dir/river/send-and-focus-output.s
 # Send the focused view to a specific output
 riverctl map normal Alt S spawn "$config_dir/river/send-to-output.sh"
 
+# Move views with mouse
+riverctl map-pointer normal Shift BTN_LEFT move-view
+
+# Resize views with mouse
+riverctl map-pointer normal Shift BTN_RIGHT resize-view
+
+# Tag mappings
+#-------------------------------------------------------------------------------
+
 # Cycle focused tags
 # see: https://gitlab.com/akumar-xyz/river-shifttags/-/tree/master
 riverctl map normal Alt Tab spawn "river-shifttags"
 riverctl map normal Alt Backspace spawn "river-shifttags --shift -1"
 
-# Manage specific tags
 for i in $(seq 1 9); do
     # NOTE: Unconventional formatting here is to avoid a bug in tree-sitter-bash when parsing `<<`.
     # see: https://github.com/tree-sitter/tree-sitter-bash
@@ -93,11 +119,8 @@ for i in $(seq 1 9); do
     riverctl map normal Alt+Control "$i" toggle-focused-tags $tags
 done
 
-# Bump the focused view to the top of the layout stack
-riverctl map normal Alt Z zoom
-
-# Toggle fullscreen
-riverctl map normal Alt F toggle-fullscreen
+# Layout mappings
+#-------------------------------------------------------------------------------
 
 # Change layout orientation
 riverctl map normal Alt Right send-layout-cmd rivercarro "main-location right"
@@ -109,15 +132,12 @@ riverctl map normal Alt Up send-layout-cmd rivercarro "main-location top"
 riverctl map normal Alt Page_Down send-layout-cmd rivercarro "main-ratio +0.05"
 riverctl map normal Alt Page_Up send-layout-cmd rivercarro "main-ratio -0.05"
 
-# Take a screenshot of a selected rectangle
+# Miscellaneous mappings
+#-------------------------------------------------------------------------------
+
+# Take screenshot of a selected rectangle
 riverctl map normal Alt P spawn "$scripts_dir/screenshot-rectangle.sh"
 riverctl map normal Alt+Shift P spawn "$scripts_dir/screenshot-rectangle.sh --upload"
 
-# Toggle window float
-riverctl map normal Alt Space toggle-float
-
-# Move views with mouse
-riverctl map-pointer normal Shift BTN_LEFT move-view
-
-# Resize views with mouse
-riverctl map-pointer normal Shift BTN_RIGHT resize-view
+# Dismiss notifications
+riverctl map normal Alt D spawn "makoctl dismiss -a"
---
+++ .local/scripts/open-terminal-cwd.sh
@@ -0,0 +1,38 @@
+#-------------------------------------------------------------------------------
+# Open a new terminal and change the working directory.
+#-------------------------------------------------------------------------------
+
+set -euo pipefail
+
+# Imports
+#-------------------------------------------------------------------------------
+
+# shellcheck disable=SC1091
+source "$XDG_OPT_HOME/shell-utils/debug.sh"
+
+# Validation
+#-------------------------------------------------------------------------------
+
+assert_dependency riverctl
+assert_dependency wofi
+
+# Choose the working directory
+#-------------------------------------------------------------------------------
+
+declare -A directories=(
+    ["dotfiles"]="$HOME/Code/dotfiles"
+    ["Code"]="$HOME/Code"
+    ["Documents"]="$HOME/Documents"
+    ["Downloads"]="$HOME/Downloads"
+    ["Media"]="$HOME/Media"
+    ["Projects"]="$HOME/Projects"
+    ["Wiki"]="$HOME/Wiki"
+)
+
+declare target
+target=$(printf "%s\n" "${!directories[@]}" | wofi --dmenu 2> /dev/null)
+
+# Spawn a new terminal
+#-------------------------------------------------------------------------------
+
+riverctl spawn "alacritty --working-directory=${directories[$target]}"
