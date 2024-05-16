ff15581f3149d37b8d0fedc72e675a8ea7b15769
Casper <draakie@proton.me>
2024-05-16 18:21:15 +0200

    scripts: Display line numbers with view.sh when viewing a file.

--- .local/scripts/view.sh
+++ .local/scripts/view.sh
@@ -8,6 +8,7 @@ set -euo pipefail
 # stdin is a tty: Process command line arguments.
 if [ -t 0 ]; then
     file_name="$1"
+    style="numbers"
 
     input="$("$XDG_SCRIPTS_HOME/format-text.sh" "$file_name")"
     input_width="$(wc -L "$file_name" | cut -d' ' -f1)"
@@ -15,6 +16,7 @@ if [ -t 0 ]; then
 # stdin is not a tty: Process standard input.
 else
     stdin="$(cat)"
+    style="plain"
 
     input="$stdin"
     input_width="$(echo "$stdin" | "$XDG_OPT_HOME/shell-utils/strip-ansi.sh" | wc -L)"
@@ -27,11 +29,11 @@ terminal_width=$(( $(stty -F /dev/tty size | cut -d' ' -f2)-1 ))
 
 # If the text is too wide or too tall, page it.
 if [[ $input_lines -gt $terminal_height || $input_width -gt $terminal_width ]]; then
-    bat --force-colorization --style=plain --paging=never --wrap=never <(echo "$input") \
+    bat --force-colorization --style=$style --paging=never --wrap=never <(echo "$input") \
         | less --chop-long-lines
 
 # Otherwise, display the text in the terminal.
 else
-    bat --force-colorization --style=plain --paging=never --wrap=never <(echo "$input")
+    bat --force-colorization --style=$style --paging=never --wrap=never <(echo "$input")
 
 fi
