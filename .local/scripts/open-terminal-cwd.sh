3482e77c628c856606fa553fdc39ac4c34f6c889
Casper <draakie@proton.me>
2024-10-29 15:06:28 +0100

    niri: Restore alacritty as default terminal; wezterm is too buggy.

--- .local/scripts/open-terminal-cwd.sh
+++ .local/scripts/open-terminal-cwd.sh
@@ -27,4 +27,4 @@ target=$(printf "%s\n" "$(zoxide query --list)" | wofi -p "Open terminal at..." 
 # Spawn a new terminal
 #-------------------------------------------------------------------------------
 
-niri msg action spawn -- "sh" "-c" "wezterm start --cwd $target"
+niri msg action spawn -- "sh" "-c" "alacritty --working-directory $target"
