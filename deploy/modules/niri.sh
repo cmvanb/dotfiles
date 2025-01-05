7d5eb14316b4acd5a707d29b4665a96d08c9467b
Casper <draakie@proton.me>
2025-01-05 12:30:50 +0100

    deploy: Extract niri portal config to module.

--- deploy/modules/niri.sh
+++ deploy/modules/niri.sh
@@ -14,6 +14,14 @@ source "$base_dir/local/opt/shell-utils/fs.sh"
 niri::install () {
     echo "└> Installing niri configuration."
 
+    if [[ $host == "supertubes" || $host == "cyxwel" ]]; then
+        force_link "$base_dir/config/niri/workspace.sh~home-triple" "$config_dir/niri/workspace.sh"
+
+    else
+        echo "[$(basename "$0")] ERROR: \`$host\` is not accounted for."
+        exit 1
+    fi
+
     mkdir -p "$config_dir/niri"
     force_link "$base_dir/config/niri/config.kdl" "$config_dir/niri/config.kdl"
     force_link "$base_dir/config/niri/focus-window.sh" "$config_dir/niri/focus-window.sh"
@@ -22,13 +30,8 @@ niri::install () {
     force_link "/usr/lib/systemd/user/mako.service" "$config_dir/systemd/user/niri.service.wants/mako.service"
     force_link "/usr/lib/systemd/user/waybar.service" "$config_dir/systemd/user/niri.service.wants/waybar.service"
 
-    if [[ $host == "supertubes" || $host == "cyxwel" ]]; then
-        force_link "$base_dir/config/niri/workspace.sh~home-triple" "$config_dir/niri/workspace.sh"
-
-    else
-        echo "[$(basename "$0")] ERROR: \`$host\` is not accounted for."
-        exit 1
-    fi
+    mkdir -p "$config_dir/xdg-desktop-portal"
+    force_link "$base_dir/config/niri/niri-portals.conf" "$config_dir/xdg-desktop-portal/niri-portals.conf"
 }
 
 niri::uninstall () {
