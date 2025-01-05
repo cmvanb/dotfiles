f73753a82ed37ecaa10436e9fb0afb6e1bf36d15
Casper <draakie@proton.me>
2025-01-05 22:46:21 +0100

    deploy: Extract shell utility scripts to module.

--- deploy/modules/python.sh
+++ deploy/modules/python.sh
@@ -6,6 +6,7 @@
 script_dir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 base_dir=$(realpath "$script_dir/../..")
 config_dir=${XDG_CONFIG_HOME:-$HOME/.config}
+scripts_dir=${XDG_SCRIPTS_HOME:-$HOME/.local/scripts}
 
 source "$base_dir/local/opt/shell-utils/fs.sh"
 
@@ -14,6 +15,8 @@ python::install () {
     echo "└> Installing python configuration."
 
     force_link "$base_dir/config/pip" "$config_dir/pip"
+
+    force_link "$base_dir/local/scripts/generate-venv.sh" "$scripts_dir/generate-venv.sh"
 }
 
 python::uninstall () {
