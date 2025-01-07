b996a58abcc29b88dd53f85205027eb891a472d6
Casper <draakie@proton.me>
2025-01-07 23:18:59 +0100

    deploy: Move hyprland configuration.

--- deploy/modules/hyprland.sh
+++ deploy/modules/hyprland.sh
@@ -12,18 +12,19 @@ source "$base_dir/config/lib-shell-utils/fs.sh"
 hyprland::install () {
     echo "└> Installing hyprland configuration."
 
-    force_link "$base_dir/config/hypr" "$XDG_CONFIG_HOME/hypr"
+    ensure_directory "$XDG_CONFIG_HOME/hypr"
+    force_link "$base_dir/config/hyprland/hyprland.conf" "$XDG_CONFIG_HOME/hypr/hyprland.conf"
 
     echo "└> Installing hyprland shortcuts."
 
-    mkdir -p "$XDG_BIN_HOME"
-    force_link "$base_dir/local/bin/init~hyprland" "$XDG_BIN_HOME/init"
+    ensure_directory "$XDG_BIN_HOME"
+    force_link "$base_dir/config/hyprland/init~hyprland" "$XDG_BIN_HOME/init"
 }
 
 hyprland::uninstall () {
     echo "└> Uninstalling hyprland configuration."
 
-    rm "$XDG_CONFIG_HOME/hypr"
+    rm -r "$XDG_CONFIG_HOME/hypr"
 
     echo "└> Uninstalling hyprland shortcuts."
 
