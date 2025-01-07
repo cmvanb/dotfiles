165603800cbc366865fc888f7403574603eafbc6
Casper <draakie@proton.me>
2025-01-07 22:13:10 +0100

    deploy: Move qutebrowser scripts and assorted config files.

--- deploy/modules/qutebrowser.sh
+++ deploy/modules/qutebrowser.sh
@@ -17,24 +17,25 @@ qutebrowser::install () {
     force_link "$base_dir/config/qutebrowser/stylesheet.css" "$XDG_CONFIG_HOME/qutebrowser/stylesheet.css"
 
     mkdir -p "$XDG_DATA_HOME/applications"
-    force_link "$base_dir/local/share/applications/org.qutebrowser.qutebrowser.desktop" "$XDG_DATA_HOME/applications/org.qutebrowser.qutebrowser.desktop"
+    force_link "$base_dir/config/qutebrowser/org.qutebrowser.qutebrowser.desktop" "$XDG_DATA_HOME/applications/org.qutebrowser.qutebrowser.desktop"
 
     mkdir -p "$XDG_DATA_HOME/qutebrowser/userscripts"
-    force_link "$base_dir/local/share/qutebrowser/userscripts/format_json.sh" "$XDG_DATA_HOME/qutebrowser/userscripts/format_json.sh"
-    force_link "$base_dir/local/share/qutebrowser/userscripts/readability.py" "$XDG_DATA_HOME/qutebrowser/userscripts/readability.py"
+    force_link "$base_dir/config/qutebrowser/userscripts/format_json.sh" "$XDG_DATA_HOME/qutebrowser/userscripts/format_json.sh"
+    force_link "$base_dir/config/qutebrowser/userscripts/readability.py" "$XDG_DATA_HOME/qutebrowser/userscripts/readability.py"
 
     mkdir -p "$XDG_SCRIPTS_HOME"
-    force_link "$base_dir/local/scripts/add-bookmark.sh" "$XDG_SCRIPTS_HOME/add-bookmark.sh"
-    force_link "$base_dir/local/scripts/select-bookmark.sh" "$XDG_SCRIPTS_HOME/select-bookmark.sh"
-    force_link "$base_dir/local/scripts/open-qutebrowser-session.sh" "$XDG_SCRIPTS_HOME/open-qutebrowser-session.sh"
+    force_link "$base_dir/config/qutebrowser/add-bookmark.sh" "$XDG_SCRIPTS_HOME/add-bookmark.sh"
+    force_link "$base_dir/config/qutebrowser/browse.sh" "$XDG_SCRIPTS_HOME/browse.sh"
+    force_link "$base_dir/config/qutebrowser/open-qutebrowser-session.sh" "$XDG_SCRIPTS_HOME/open-qutebrowser-session.sh"
+    force_link "$base_dir/config/qutebrowser/select-bookmark.sh" "$XDG_SCRIPTS_HOME/select-bookmark.sh"
 
     mkdir -p "$XDG_TEMPLATES_DIR"
-    force_link "$base_dir/local/share/templates/bookmark.md~esh" "$XDG_TEMPLATES_DIR/bookmark.md~esh"
+    force_link "$base_dir/config/qutebrowser/bookmark.md~esh" "$XDG_TEMPLATES_DIR/bookmark.md~esh"
 
     echo "└> Installing qutebrowser shortcuts."
 
     mkdir -p "$XDG_BIN_HOME"
-    force_link "$base_dir/local/bin/browse" "$XDG_BIN_HOME/browse"
+    force_link "$base_dir/config/qutebrowser/browse.sh" "$XDG_BIN_HOME/browse"
 }
 
 qutebrowser::uninstall () {
