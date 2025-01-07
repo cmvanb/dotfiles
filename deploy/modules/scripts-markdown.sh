29a76b48c5fa252fb3250f0738d0d928944a27df
Casper <draakie@proton.me>
2025-01-07 22:44:18 +0100

    deploy: Move markdown scripts.

--- deploy/modules/scripts-markdown.sh
+++ deploy/modules/scripts-markdown.sh
@@ -11,8 +11,8 @@ scripts-markdown::install () {
     echo "└> Installing markdown scripts."
 
     mkdir -p "$XDG_SCRIPTS_HOME"
-    force_link "$base_dir/local/scripts/markdown-to-html.sh" "$XDG_SCRIPTS_HOME/markdown-to-html.sh"
-    force_link "$base_dir/local/scripts/preview-markdown.sh" "$XDG_SCRIPTS_HOME/preview-markdown.sh"
+    force_link "$base_dir/config/scripts-markdown/markdown-to-html.sh" "$XDG_SCRIPTS_HOME/markdown-to-html.sh"
+    force_link "$base_dir/config/scripts-markdown/preview-markdown.sh" "$XDG_SCRIPTS_HOME/preview-markdown.sh"
 }
 
 scripts-markdown::uninstall () {
