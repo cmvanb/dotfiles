1729d201d541d335e303ac96a60788003770a059
Casper <draakie@proton.me>
2025-09-12 11:28:13 +0200

    deploy: Don't use non-portable cd flag.

--- deploy/modules/bitwarden.sh
+++ deploy/modules/bitwarden.sh
@@ -12,7 +12,7 @@ bitwarden::install () {
 
     ensure_directory "$XDG_CONFIG_HOME/Bitwarden"
     # NOTE: Don't symlink Bitwarden config because it will be overwritten by the app.
-    cp -n "$base_dir/config/bitwarden/data.json" "$XDG_CONFIG_HOME/Bitwarden/data.json" && true
+    cp --update=none "$base_dir/config/bitwarden/data.json" "$XDG_CONFIG_HOME/Bitwarden/data.json" && true
 
     ensure_directory "$XDG_DATA_HOME/applications"
     force_link "$base_dir/config/bitwarden/bitwarden.desktop" "$XDG_DATA_HOME/applications/bitwarden.desktop"
