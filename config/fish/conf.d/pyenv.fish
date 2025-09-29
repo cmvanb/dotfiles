08c45a690eab68d4b9cf5f79c868f8f44e86d016
Casper <draakie@proton.me>
2025-09-29 23:01:32 +0200

    fish: Improve shell startup time by lazy loading pyenv.

--- config/fish/conf.d/pyenv.fish
+++ config/fish/conf.d/pyenv.fish
@@ -7,5 +7,11 @@ fish_add_path -pP $PYENV_ROOT/bin
 
 if status is-interactive
 and command -v pyenv &> /dev/null
-    pyenv init - | source
+    # Lazy load pyenv init.
+    # see: https://github.com/pyenv/pyenv/issues/2918
+    function pyenv
+        functions -e pyenv
+        command pyenv init - fish | source
+        pyenv $argv
+    end
 end
