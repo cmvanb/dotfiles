4f2a752a240491fed86bed5a0b8c51083ef1da80
Casper <draakie@proton.me>
2026-02-11 02:13:40 +0100

    sway: Run browser session on startup.

--- deploy/modules/sway.sh
+++ deploy/modules/sway.sh
@@ -21,8 +21,8 @@ sway::install () {
     force_link "$base_dir/config/sway/sws.sh" "$XDG_CONFIG_HOME/sway/sws.sh"
 
     if [[ $host == "casino" ]]; then
-        force_link "$base_dir/config/sway/outputs.conf~home-dual" "$XDG_CONFIG_HOME/sway/outputs.conf"
-        force_link "$base_dir/config/sway/workspace.conf~home-dual" "$XDG_CONFIG_HOME/sway/workspace.conf"
+        force_link "$base_dir/config/sway/outputs.conf~home-laptop" "$XDG_CONFIG_HOME/sway/outputs.conf"
+        force_link "$base_dir/config/sway/workspace.conf~home-laptop" "$XDG_CONFIG_HOME/sway/workspace.conf"
 
     elif [[ $host == "cyxwel" ]]; then
         force_link "$base_dir/config/sway/outputs.conf~home-dual" "$XDG_CONFIG_HOME/sway/outputs.conf"
