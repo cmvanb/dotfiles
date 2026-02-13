fb2c3122bffbde04fdf3e08e5942205ce6c3bae7
Casper <casper@codemu.ch>
2026-02-13 18:04:11 +0100

    disable-xdg-desktop-files: Update electron version.

--- modules/disable-xdg-desktop-files/deploy.sh
+++ modules/disable-xdg-desktop-files/deploy.sh
@@ -9,7 +9,6 @@ base_dir=$(realpath "$script_dir/../..")
 source "$base_dir/modules/lib-shell-utils/src/fs.sh"
 
 
-    # TODO: Update to electron32.
 disable-xdg-desktop-files::install () {
     echo "└> Installing disable-xdg-desktop-files configuration."
 
@@ -19,7 +18,7 @@ disable-xdg-desktop-files::install () {
     force_link "$src/bssh.desktop" "$XDG_DATA_HOME/applications/bssh.desktop"
     force_link "$src/bvnc.desktop" "$XDG_DATA_HOME/applications/bvnc.desktop"
     force_link "$src/cmake-gui.desktop" "$XDG_DATA_HOME/applications/cmake-gui.desktop"
-    force_link "$src/electron24.desktop" "$XDG_DATA_HOME/applications/electron24.desktop"
+    force_link "$src/electron36.desktop" "$XDG_DATA_HOME/applications/electron36.desktop"
     force_link "$src/lstopo.desktop" "$XDG_DATA_HOME/applications/lstopo.desktop"
     force_link "$src/qv4l2.desktop" "$XDG_DATA_HOME/applications/qv4l2.desktop"
     force_link "$src/qvidcap.desktop" "$XDG_DATA_HOME/applications/qvidcap.desktop"
@@ -32,7 +31,7 @@ disable-xdg-desktop-files::uninstall () {
     rm "$XDG_DATA_HOME/applications/bssh.desktop"
     rm "$XDG_DATA_HOME/applications/bvnc.desktop"
     rm "$XDG_DATA_HOME/applications/cmake-gui.desktop"
-    rm "$XDG_DATA_HOME/applications/electron24.desktop"
+    rm "$XDG_DATA_HOME/applications/electron36.desktop"
     rm "$XDG_DATA_HOME/applications/lstopo.desktop"
     rm "$XDG_DATA_HOME/applications/qv4l2.desktop"
     rm "$XDG_DATA_HOME/applications/qvidcap.desktop"
