7578d738f73d1a805e35a27f57022135bbe5ba59
Casper <casper@codemu.ch>
2026-03-01 18:44:00 +0100

    qutebrowser: Custom CSS for arch wiki.

--- modules/qutebrowser/src/stylemap.py
+++ modules/qutebrowser/src/stylemap.py
@@ -1,5 +1,6 @@
 styles = {
     'arch-linux-forum.css': ['*bbs.archlinux.org*'],
+    'arch-linux-wiki.css': ['*wiki.archlinux.org*'],
     'hackernews.css': ['*news.ycombinator.com*'],
     'github.css': ['*github.com*'],
     'wikipedia.css': ['*wikipedia.org*'],
