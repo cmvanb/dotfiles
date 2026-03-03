6ce7b8090c9ae51987cd519520b9ab9f5b3f295b
Casper <casper@codemu.ch>
2026-03-03 14:34:08 +0100

    sway: Auto start bitwarden.

--- modules/sway/src/startup-workspace.sh
+++ modules/sway/src/startup-workspace.sh
@@ -5,6 +5,7 @@
 # Opens startup workspaces and focuses workspace 1. Exec'd by sway at startup.
 #-------------------------------------------------------------------------------
 
-swaymsg 'workspace todo; exec spawn-terminal.sh --working-directory ~/Wiki/todo --command edit'
 swaymsg 'workspace home; exec bash -c "nm-online -qs && sleep 1 && qutebrowser -r home"'
+swaymsg 'workspace todo; exec spawn-terminal.sh --working-directory ~/Wiki/todo --command edit'
+swaymsg 'workspace auth; exec Bitwarden'
 swaymsg workspace 1
