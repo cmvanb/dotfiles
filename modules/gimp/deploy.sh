737c409f2f29a1e1b8aa694723bba1276b704f50
Casper <casper@codemu.ch>
2026-03-06 00:15:34 +0100

    gimp: Configure gimp to use custom theme.

---
+++ modules/gimp/deploy.sh
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Deploy gimp configuration
+#-------------------------------------------------------------------------------
+
+
+script_dir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+base_dir=$(realpath "$script_dir/../..")
+
+source "$base_dir/lib/fs.sh"
+
+
+gimp::install () {
+    echo "└> Installing gimp configuration."
+
+    local src="$base_dir/modules/gimp/src"
+
+    ensure_directory "$XDG_CONFIG_HOME/GIMP/3.0"
+    force_link "$src/gimprc" "$XDG_CONFIG_HOME/GIMP/3.0/gimprc"
+}
+
+gimp::uninstall () {
+    echo "└> Uninstalling gimp configuration."
+
+    rm "$XDG_CONFIG_HOME/GIMP/3.0/gimprc"
+}
