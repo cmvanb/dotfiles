5d9f6925f76773caa3c271fadc864fbde4521d11
Casper <casper@codemu.ch>
2026-03-19 02:04:06 +0100

    wireplumber: Configure preferred audio devices.

---
+++ modules/wireplumber/deploy.sh
@@ -0,0 +1,39 @@
+#-------------------------------------------------------------------------------
+# Deploy wireplumber configuration
+#-------------------------------------------------------------------------------
+
+
+script_dir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+base_dir=$(realpath "$script_dir/../..")
+
+source "$base_dir/lib/fs.sh"
+
+
+wireplumber::install () {
+    echo "└> Installing wireplumber configuration."
+
+    local src="$base_dir/modules/wireplumber/src"
+
+    ensure_directory "$XDG_CONFIG_HOME/wireplumber/wireplumber.conf.d"
+
+    force_link "$src/wireplumber.conf.d/50-audio-priority.conf" \
+        "$XDG_CONFIG_HOME/wireplumber/wireplumber.conf.d/50-audio-priority.conf"
+}
+
+wireplumber::uninstall () {
+    echo "└> Uninstalling wireplumber configuration."
+
+    rm -f "$XDG_CONFIG_HOME/wireplumber/wireplumber.conf.d/50-audio-priority.conf"
+}
+
+wireplumber::enable () {
+    echo "└> Enabling wireplumber user service."
+
+    systemctl --user enable wireplumber
+}
+
+wireplumber::disable () {
+    echo "└> Disabling wireplumber user service."
+
+    systemctl --user disable wireplumber
+}
