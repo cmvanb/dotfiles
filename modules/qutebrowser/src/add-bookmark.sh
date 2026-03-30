52e395c780878ca63fdab822228318e635680625
Casper <casper@codemu.ch>
2026-03-30 15:59:39 +0200

    qutebrowser: Improve add-bookmark script.

--- modules/qutebrowser/src/add-bookmark.sh
+++ modules/qutebrowser/src/add-bookmark.sh
@@ -13,6 +13,17 @@ source "$XDG_OPT_HOME/shell-utils/debug.sh"
 # shellcheck disable=1091
 source "$XDG_OPT_HOME/shell-utils/name-formatting.sh"
 
+# Helpers
+#-------------------------------------------------------------------------------
+
+_script=$(basename "$0")
+
+err() {
+    local msg="$*"
+    echo "[$_script] ERROR: $msg" >&2
+    notify-send "$_script" "$msg"
+}
+
 # Validation
 #-------------------------------------------------------------------------------
 
@@ -22,7 +33,7 @@ bookmark_template="$XDG_TEMPLATES_DIR/bookmark.mako.md"
 bookmark_dir="$HOME/Wiki/bookmarks"
 
 if [[ ! -f $bookmark_template ]]; then
-    echo "[$(basename "$0")] ERROR: Missing template: \`$bookmark_template\`"
+    err "Missing template: \`$bookmark_template\`"
     exit 1
 fi
 
@@ -30,13 +41,14 @@ fi
 #-------------------------------------------------------------------------------
 
 input_name="${1:-New Bookmark}"
-input_url="${2:-https://}"
+input_url="${2:-}"
 
 # Open a form dialog to get user input for bookmark data
 #-------------------------------------------------------------------------------
 
 if ! form_input=$(yad --form \
     --width=640 \
+    --separator=$'\x1F' \
     --title="Add bookmark" \
     --field="Name" "$input_name" \
     --field="URL" "$input_url" \
@@ -45,28 +57,26 @@ if ! form_input=$(yad --form \
     --button="Cancel":1 \
     );
 then
-    echo "[$(basename "$0")] ERROR: User cancelled script."
-    exit 1
+    exit 0
 fi
 
 # Process and validate the form input
 #-------------------------------------------------------------------------------
+new_bookmark_name=$(echo "$form_input" | cut -d $'\x1F' -f 1)
+new_bookmark_url=$(echo "$form_input" | cut -d $'\x1F' -f 2)
+new_bookmark_tags_raw=$(echo "$form_input" | cut -d $'\x1F' -f 3)
 
 # Validate name.
-new_bookmark_name=$(echo "$form_input" | cut -d "|" -f 1)
-
 if [[ -z "$new_bookmark_name" ]]; then
-    echo "[$(basename "$0")] ERROR: Missing or bad input for bookmark name."
+    err "Missing or bad input for bookmark name."
     exit 20
 fi
 
 new_bookmark_name_kebab=$(convert_to_kebab_case "$new_bookmark_name")
 
 # Validate URL.
-new_bookmark_url=$(echo "$form_input" | cut -d "|" -f 2)
-
 if [[ -z "$new_bookmark_url" ]]; then
-    echo "[$(basename "$0")] ERROR: Missing or bad input for bookmark url."
+    err "Missing or bad input for bookmark url."
     exit 21
 fi
 
@@ -74,9 +84,9 @@ fi
 declare -a input_tags=()
 declare -a new_bookmark_tags=()
 
-IFS=' ' read -r -a input_tags <<< "$(echo "$form_input" | cut -d "|" -f 3)"
+IFS=' ' read -r -a input_tags <<< "$new_bookmark_tags_raw"
 
-for t in "${input_tags[@]}"; do
+for t in "${input_tags[@]+"${input_tags[@]}"}"; do
     # TODO: Ignore duplicate tags.
     # Ignore 1-char tags and empty tags.
     if [[ "${#t}" -gt 1 ]] && [[ -n "$t"  ]]; then
@@ -84,8 +94,8 @@ for t in "${input_tags[@]}"; do
     fi
 done
 
-if [[ -z "$new_bookmark_tags" ]]; then
-    echo "[$(basename "$0")] ERROR: Missing or bad input for bookmark tags."
+if [[ ${#new_bookmark_tags[@]} -eq 0 ]]; then
+    err "Missing or bad input for bookmark tags."
     exit 22
 fi
 
@@ -94,22 +104,20 @@ bookmark_file_path="$bookmark_dir/$new_bookmark_name_kebab.md"
 
 # NOTE: Should we also check the URL being already being bookmarked?
 if [[ -f "$bookmark_file_path" ]]; then
-    echo "[$(basename "$0")] ERROR: Bookmark already exists: \`$bookmark_file_path\`"
+    err "Bookmark already exists: \`$bookmark_file_path\`"
     exit 23
 fi
 
 # Create a bookmark
 #-------------------------------------------------------------------------------
 
-# echo "$new_bookmark_name"
-# echo "$new_bookmark_name_kebab"
-# echo "$new_bookmark_url"
-# echo "${new_bookmark_tags[*]}"
-
-python3 "$XDG_OPT_HOME/theme/template.py" \
+if ! render-mako \
     "$bookmark_template" \
     "$bookmark_file_path" \
     new_bookmark_name="$new_bookmark_name" \
     new_bookmark_name_kebab="$new_bookmark_name_kebab" \
     new_bookmark_url="$new_bookmark_url" \
-    new_bookmark_tags="${new_bookmark_tags[*]}"
+    new_bookmark_tags="${new_bookmark_tags[*]+"${new_bookmark_tags[*]}"}";  then
+    err "Failed to create bookmark file: \`$bookmark_file_path\`"
+    exit 30
+fi
