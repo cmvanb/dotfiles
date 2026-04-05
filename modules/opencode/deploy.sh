8c3a0a5f29d2ca61658206e47863eb301bea7b62
Casper <casper@codemu.ch>
2026-04-05 15:40:27 +0200

    opencode: Use correct config schema.

--- modules/opencode/deploy.sh
+++ modules/opencode/deploy.sh
@@ -15,6 +15,7 @@ opencode::install () {
 
     ensure_directory "$XDG_CONFIG_HOME/opencode"
     force_link "$src/opencode.json" "$XDG_CONFIG_HOME/opencode/opencode.json"
+    force_link "$src/tui.json" "$XDG_CONFIG_HOME/opencode/tui.json"
 
     ensure_directory "$XDG_CONFIG_HOME/opencode/themes"
     template::render_mako "$src/themes/carbon-dark.mako.json" "$XDG_CONFIG_HOME/opencode/themes/carbon-dark.json"
@@ -24,5 +25,6 @@ opencode::uninstall () {
     echo "└> Uninstalling opencode configuration."
 
     rm "$XDG_CONFIG_HOME/opencode/opencode.json"
+    rm "$XDG_CONFIG_HOME/opencode/tui.json"
     rm "$XDG_CONFIG_HOME/opencode/themes/carbon-dark.json"
 }
