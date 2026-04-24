d98012ada1085e02357924306e326a6947847325
Casper <casper@codemu.ch>
2026-04-24 16:38:15 +0200

    sway: Configure workspace for mail client.

--- modules/sway/src/startup-workspace.sh
+++ modules/sway/src/startup-workspace.sh
@@ -8,4 +8,5 @@
 swaymsg 'workspace home; exec bash -c "nm-online -qs && sleep 1 && qutebrowser -r home"'
 swaymsg 'workspace todo; exec spawn-terminal.sh --working-directory ~/Wiki/todo --command edit'
 swaymsg 'workspace auth; exec Bitwarden'
+swaymsg 'workspace mail; exec geary'
 swaymsg workspace 1
