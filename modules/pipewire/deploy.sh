8d65954c29e756b08d7808f10f961554594a8ed1
Casper <casper@codemu.ch>
2026-09-17 03:03:58 +0200

    pipewire: Route ALSA default to PipeWire.

--- modules/pipewire/deploy.sh
+++ modules/pipewire/deploy.sh
@@ -3,6 +3,28 @@
 #-------------------------------------------------------------------------------
 
 
+script_dir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+base_dir=$(realpath "$script_dir/../..")
+
+source "$base_dir/lib/fs.sh"
+
+
+pipewire::install () {
+    echo "└> Installing pipewire configuration."
+
+    local src="$base_dir/modules/pipewire/src"
+
+    fs::ensure_directory "$XDG_CONFIG_HOME/alsa"
+    fs::force_link "$src/asoundrc" "$XDG_CONFIG_HOME/alsa/asoundrc"
+}
+
+pipewire::uninstall () {
+    echo "└> Uninstalling pipewire configuration."
+
+    rm -f "$XDG_CONFIG_HOME/alsa/asoundrc"
+    rmdir --ignore-fail-on-non-empty "$XDG_CONFIG_HOME/alsa"
+}
+
 pipewire::enable () {
     echo "└> Enabling pipewire user service."
 
