713a0cd33e5b06e36b149f436f1054d191384363
Casper <casper@codemu.ch>
2026-09-22 10:55:38 +0200

    scripts-misc: Force mjpeg 1600x1200 for show-webcam.

--- modules/scripts-misc/src/show-webcam.sh
+++ modules/scripts-misc/src/show-webcam.sh
@@ -3,4 +3,4 @@
 # Show webcam video using ffplay
 #-------------------------------------------------------------------------------
 
-ffplay -fflags nobuffer /dev/video0 > /dev/null 2>&1 &
+ffplay -fflags nobuffer -input_format mjpeg -video_size 1600x1200 /dev/video0 > /dev/null 2>&1 &
